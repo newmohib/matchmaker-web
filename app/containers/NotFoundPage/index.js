9bf92ea0d8643f9cefc52074db4ccf4a013ef9b9
unknown <newmohib@gmail.com>
2019-10-14 21:11:38 +0600

    update not found page

--- app/containers/NotFoundPage/index.js
+++ app/containers/NotFoundPage/index.js
@@ -17,7 +17,7 @@ export default function NotFound() {
     style={{ height: 'calc(100vh - 65px)' }}
   >
     <div className="row justify-content-center h-100">
-      <div className="col-10 col-lg-6 col-md-8 col-sm-10 h-100 d-flex align-items-center">
+      <div className="col-12 col-lg-6 col-md-12 col-sm-12 h-100 d-flex align-items-center">
         <div className="form-container formStyle">
           <div className="container">
           <h1>
